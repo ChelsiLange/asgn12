727ee97c02f6dc1b0243ef9ec437db0afbd16fc8
Chelsi Lange <langechelsi@gmail.com>
2021-12-09 06:31:52 -0500

    begins asgn-12

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Image Upload</title>
+  </head>
+
+  <body>
+    <form action="upload.php" method="post" enctype="multipart/form-data">
+      <input type="file" name="my_image">
+      <input type="submit" name="submit" value="Upload">
+
+    </form>
+  </body>
+</html>
